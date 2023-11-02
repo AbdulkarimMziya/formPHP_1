05740a3152ca8f710e8c9cd84b09df12ff85ee76
AbdulkarimMziya <66888226+AbdulkarimMziya@users.noreply.github.com>
2023-11-02 12:51:16 -0700

    Update index.php

--- tuts/index.php
+++ tuts/index.php
@@ -11,15 +11,35 @@
 ?> -->
 
 <?php
-    #1. How to connect to the Database
+    /***** How to connect to the Database *****/
 
-    // connect to database
-    $conn = mysqli_connect('localhost','abdul','test1234','ninja_pizza');  // ('host', 'username', 'password','DB_name')
+    #1. connect to database
+    $conn = mysqli_connect('localhost','root','','ninja_pizza');  // ('host', 'username', 'password','DB_name')
 
-    // check connection
+    #2. check connection
     if(!$conn){
         echo 'Connection error: ' . mysqli_connect_error();
     }
+
+    #3. Write query for all pizzas
+    $sql = 'SELECT title,ingredients,id FROM pizzas ORDER BY created_at';
+
+    #4. Make query & get result
+    $result = mysqli_query($conn, $sql);
+
+    #5. Fetch the resulting rows as an array
+    // Fetch all rows as associative arrays
+    $pizzas = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+
+    // free result from memory
+    mysqli_free_result($result);
+
+    // close connection
+    // mysql_close($conn);
+
+    // print_r($pizzas);
+
 
 ?>
 <!DOCTYPE html>
@@ -31,6 +51,36 @@
 </head>
 <body>
     <?php include('templates/header.php'); ?>
+
+        <h4 class="center grey-text">Pizzas!</h4>
+
+        <div class="container">
+            <div class="row">
+
+                <?php foreach ($pizzas as $pizza) : ?>
+
+                    <div class="col s6 md3">
+                        <div class="card z-depth-0">
+                            <div class="card-content center">
+                                <h6> <?php echo htmlspecialchars($pizza['title']); ?> </h6>
+                                <div>
+                                    <ul>
+                                        <?php foreach (explode(',', $pizza['ingredients']) as $ingredient) { ?>
+                                            <li> <?php echo htmlspecialchars($ingredient); ?> </li>
+                                        <?php } ?>
+                                    </ul>
+                                </div>
+                            </div>            
+                            <div class="card-action right-align">
+                                <a class="brand-text" href="#">more info</a>
+                            </div>
+                        </div> 
+                    </div>
+
+                <?php endforeach; ?>
+                
+            </div>
+        </div>
     <?php include('templates/footer.php'); ?>
     
 
